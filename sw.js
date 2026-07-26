c2664d3fe71fec9119bafdd6bb32e6a5a3ae81eb
bargaouitarek-gif <bargaouitarek@gmail.com>
2026-07-26 05:34:57 +0200

    Renouveler le cache pour le nouvel accueil

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="tbr-2026-07-26-dco-command-v6";
+const VERSION="tbr-2026-07-26-home-month-v7";
 const CACHE_NAME=`${VERSION}-offline`;
 const SCOPE_PATH=new URL(self.registration.scope).pathname.replace(/\/$/,"");
 const scoped=path=>`${SCOPE_PATH}${path.startsWith("/")?path:"/"+path}`||"/";
